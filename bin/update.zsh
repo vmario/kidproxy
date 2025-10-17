5241228af1989669727f47101b896cb07aa147b1
Mariusz Chilmon <mariusz.chilmon@ctm.gdynia.pl>
2025-10-17 14:09:30 +0200

    Switch filters

--- bin/update.zsh
+++ bin/update.zsh
@@ -1,4 +1,29 @@
-#!/usr/bin/evn zsh
+#!/usr/bin/env zsh
+
+KIDPROXY="$HOME/.local/share/kidproxy"
+TINYPROXY="$HOME/.config/tinyproxy"
+
+filter() {
+	echo "Filter $1"
+
+	filter=$KIDPROXY/config/tinyproxy/filters-$1
+	link=$TINYPROXY/filters
+
+	if [[ "$(readlink $link)" == "$filter" ]]; then
+		return
+	fi
+
+	ln -sfv $filter $link
+	systemctl --user reload tinyproxy
+}
 
 cd ${0:A:h}/..
 git pull
+
+policyTimestamp=$(date -d "$(</tmp/testdate)" +%s)
+currentTimestamp=$(date +%s)
+if (( $currentTimestamp < $policyTimestamp )); then
+	filter maximum
+else
+	filter minimum
+fi
